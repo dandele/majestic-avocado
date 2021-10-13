7696bddc538cdd35dbdff984b48b040e1c9c1862
Stackbit Code Editor <projects@stackbit.com>
2021-10-13 12:43:24 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -84,7 +84,7 @@ export default class HeroSection extends React.Component {
                                 })}
                             >
                                 {title && <h1 className="hero__title">{'Ciao,'}</h1>}
-                                {<span style="" }
+                                {<span style="color: red" }
                                 {subtitle && <p className="hero__subtitle">{subtitle}</p>}
                                 {content && <div className="hero__copy">{markdownify(content)}</div>}
                                 {!_.isEmpty(actions) && (
